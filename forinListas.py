4b65511c97bd3e975502d64fdec2a0098bdf370b
Pedro Costa Nunes <89865889+PeppyCorn@users.noreply.github.com>
2025-04-15 16:32:18 -0400

    For in Lista Python
    estou compartilhando um código que se trata de for in em listas

---
+++ forinListas.py
@@ -0,0 +1,5 @@
+lista = ['Pedro', 'João', 'Lucas', 'Maria', 'Ana', 'Fernanda', 'Juliana', 'Roberta', 'Patrícia', 'Carla']
+indices = range(len(lista)) # É um range de 0 a 9, ou seja, 10 elementos
+
+for i in indices:
+    print(i, lista[i])
